1be721416d9c4576245c3245af8d2abe13554935
Oleksandr Lysun <oleksandr.lysun@teamvoy.com>
2019-06-05 18:18:09 +0300

    add di for broadcast receiver

--- app/src/main/java/com/oleksandrlysun/traveminder/TraveMinderApplication.kt
+++ app/src/main/java/com/oleksandrlysun/traveminder/TraveMinderApplication.kt
@@ -2,17 +2,22 @@ package com.oleksandrlysun.traveminder
 
 import android.app.Activity
 import android.app.Application
+import android.content.BroadcastReceiver
 import com.oleksandrlysun.traveminder.di.ApplicationModule
 import com.oleksandrlysun.traveminder.di.DaggerApplicationComponent
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
+import dagger.android.HasBroadcastReceiverInjector
 import javax.inject.Inject
 
-class TraveMinderApplication : Application(), HasActivityInjector {
+class TraveMinderApplication : Application(), HasActivityInjector, HasBroadcastReceiverInjector {
 
 	@Inject
 	lateinit var dispatchingActivityInjector: DispatchingAndroidInjector<Activity>
+
+	@Inject
+	lateinit var dispatchingBroadcastReceiverInjector: DispatchingAndroidInjector<BroadcastReceiver>
 
 	override fun onCreate() {
 		super.onCreate()
@@ -24,5 +29,9 @@ class TraveMinderApplication : Application(), HasActivityInjector {
 
 	override fun activityInjector(): AndroidInjector<Activity> {
 		return dispatchingActivityInjector
+	}
+
+	override fun broadcastReceiverInjector(): AndroidInjector<BroadcastReceiver> {
+		return dispatchingBroadcastReceiverInjector
 	}
 }
--- app/src/main/java/com/oleksandrlysun/traveminder/di/ApplicationComponent.kt
+++ app/src/main/java/com/oleksandrlysun/traveminder/di/ApplicationComponent.kt
@@ -2,6 +2,7 @@ package com.oleksandrlysun.traveminder.di
 
 import com.oleksandrlysun.traveminder.TraveMinderApplication
 import com.oleksandrlysun.traveminder.presentation.di.ActivityBindingModule
+import com.oleksandrlysun.traveminder.services.di.ServiceModule
 import dagger.Component
 import dagger.android.AndroidInjector
 import dagger.android.support.AndroidSupportInjectionModule
@@ -11,5 +12,7 @@ import javax.inject.Singleton
 @Component(modules = [
 	ApplicationModule::class,
 	ActivityBindingModule::class,
+	ServiceModule::class,
+	UtilModule::class,
 	AndroidSupportInjectionModule::class])
 interface ApplicationComponent : AndroidInjector<TraveMinderApplication>
