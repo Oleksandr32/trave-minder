c95031f6b255d353c59cfd0b683414426ed3cb0d
Oleksandr Lysun <oleksandr.lysun@teamvoy.com>
2019-06-13 17:44:50 +0300

    Camera Notes: implement search

--- app/src/main/java/com/oleksandrlysun/traveminder/presentation/screens/cameranotes/CameraNotesFragment.kt
+++ app/src/main/java/com/oleksandrlysun/traveminder/presentation/screens/cameranotes/CameraNotesFragment.kt
@@ -1,7 +1,10 @@
 package com.oleksandrlysun.traveminder.presentation.screens.cameranotes
 
+import android.view.Menu
+import android.view.MenuInflater
 import android.widget.ImageView
 import android.widget.LinearLayout
+import androidx.appcompat.widget.SearchView
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -9,6 +12,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.oleksandrlysun.traveminder.R
 import com.oleksandrlysun.traveminder.domain.models.CameraNote
 import com.oleksandrlysun.traveminder.presentation.base.PresenterFragment
+import com.oleksandrlysun.traveminder.presentation.extensions.hideSoftInput
 import com.oleksandrlysun.traveminder.presentation.extensions.visibleOrGone
 import com.oleksandrlysun.traveminder.presentation.screens.cameranotes.CameraNotesState.*
 import com.oleksandrlysun.traveminder.utils.DividerItemDecoration
@@ -38,12 +42,34 @@ class CameraNotesFragment : PresenterFragment(), CameraNotesView {
 		super.onDestroyView()
 	}
 
+	override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+		super.onCreateOptionsMenu(menu, inflater)
+		inflater.inflate(R.menu.searchable_menu, menu)
+
+		val searchView = menu.findItem(R.id.action_search)?.actionView as? SearchView
+		searchView?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+			override fun onQueryTextSubmit(query: String): Boolean {
+				return false
+			}
+
+			override fun onQueryTextChange(newText: String): Boolean {
+				presenter.search(newText)
+				return false
+			}
+		})
+	}
+
 	override fun setupUI() {
 		super.setupUI()
-		addNoteButton.setOnClickListener { presenter.addNote() }
+		addNoteButton.setOnClickListener { onAddNoteButtonClick() }
 		notesRecyclerView.layoutManager = LinearLayoutManager(context)
 		notesRecyclerView.adapter = notesAdapter
 		notesRecyclerView.addItemDecoration(DividerItemDecoration(requireContext()))
+	}
+
+	override fun setupToolbar() {
+		super.setupToolbar()
+		setHasOptionsMenu(true)
 	}
 
 	override fun setCameraNotesState(state: CameraNotesState) {
@@ -69,5 +95,10 @@ class CameraNotesFragment : PresenterFragment(), CameraNotesView {
 
 	override fun setNotes(notes: List<CameraNote>) {
 		notesAdapter.items = notes
+	}
+
+	private fun onAddNoteButtonClick() {
+		view?.hideSoftInput()
+		presenter.addNote()
 	}
 }
